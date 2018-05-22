f98eba60ed78d389440cc6542e9ec8ab677452c8
Barry Mor <barry.mo@west.com>
2018-05-22 14:15:33 +0800

    Update

--- features/environment.py
+++ features/environment.py
@@ -2,67 +2,30 @@ from appium import webdriver
 # from browserstack.local import Local
 import os, json
 
-isLocal = True
+def GetConfig(config):
+    global CONFIG, TASK_ID, BROWSERSTACK_USERNAME, BROWSERSTACK_ACCESS_KEY
+    DEFAULT_CONFIG = 'config/%s.json' % config if config else 'config/local.json'
+    CONFIG_FILE = os.environ['CONFIG_FILE'] if 'CONFIG_FILE' in os.environ else DEFAULT_CONFIG
+    TASK_ID = int(os.environ['TASK_ID']) if 'TASK_ID' in os.environ else 0
+    
+    with open(CONFIG_FILE) as data_file:
+        CONFIG = json.load(data_file)
 
-DEFAULT_CONFIG = 'config/default.json' if not isLocal else 'config/local.json'
-CONFIG_FILE = os.environ[
-    'CONFIG_FILE'] if 'CONFIG_FILE' in os.environ else DEFAULT_CONFIG
-TASK_ID = int(os.environ['TASK_ID']) if 'TASK_ID' in os.environ else 0
-
-with open(CONFIG_FILE) as data_file:
-    CONFIG = json.load(data_file)
-
-if not isLocal:
-    BROWSERSTACK_USERNAME = os.environ[
-        'BROWSERSTACK_USERNAME'] if 'BROWSERSTACK_USERNAME' in os.environ else CONFIG[
-            'user']
-    BROWSERSTACK_ACCESS_KEY = os.environ[
-        'BROWSERSTACK_ACCESS_KEY'] if 'BROWSERSTACK_ACCESS_KEY' in os.environ else CONFIG[
-            'key']
-
-# bs_local = None
-
-# def start_local():
-#     """Code to start browserstack local before start of test."""
-#     global bs_local
-#     bs_local = Local()
-#     bs_local_args = { "key": BROWSERSTACK_ACCESS_KEY, "forcelocal": "true" }
-#     bs_local.start(**bs_local_args)
-
-# def stop_local():
-#     """Code to stop browserstack local after end of test."""
-#     global bs_local
-#     if bs_local is not None:
-#         bs_local.stop()
-
-# def before_feature(context, feature):
-#     desired_capabilities = CONFIG['environments'][TASK_ID]
-
-#     for key in CONFIG["capabilities"]:
-#         if key not in desired_capabilities:
-#             desired_capabilities[key] = CONFIG["capabilities"][key]
-
-#     if "browserstack.local" in desired_capabilities and desired_capabilities["browserstack.local"]:
-#         start_local()
-
-#     context.browser = webdriver.Remote(
-#         desired_capabilities=desired_capabilities,
-#         command_executor="http://%s:%s@hub-cloud.browserstack.com/wd/hub" % (BROWSERSTACK_USERNAME, BROWSERSTACK_ACCESS_KEY)
-#     )
-
-# def after_feature(context, feature):
-#     context.browser.quit()
-#     stop_local()
-
+    if config:
+        BROWSERSTACK_USERNAME = os.environ['BROWSERSTACK_USERNAME'] if 'BROWSERSTACK_USERNAME' in os.environ else CONFIG['user']
+        BROWSERSTACK_ACCESS_KEY = os.environ['BROWSERSTACK_ACCESS_KEY'] if 'BROWSERSTACK_ACCESS_KEY' in os.environ else CONFIG['key']
 
 def before_all(context):
+    config = context.config.userdata['config'] if context.config.userdata.get('config') else None
+    GetConfig(config)
+
     desired_capabilities = CONFIG['environments'][TASK_ID]
 
     for key in CONFIG["capabilities"]:
         if key not in desired_capabilities:
             desired_capabilities[key] = CONFIG["capabilities"][key]
 
-    if (isLocal):
+    if not config:
         context.driver = webdriver.Remote(
             desired_capabilities=desired_capabilities,
             command_executor="http://localhost:4723/wd/hub")
